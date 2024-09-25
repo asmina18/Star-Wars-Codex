a97fc66c58f4d248d32d4b2fe3c50db2b95bf02f
maylova93 <142877128+maylova93@users.noreply.github.com>
2024-09-25 14:22:35 +0200

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,16 @@
+import styles from './Footer.module.scss';
 
-export  function Footer() {
+export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <p>
+          Crafted by <strong>Amina</strong>, <strong>Makka</strong>
+        </p>
+        <p>
+          <a href="#">Terms & Conditions</a> | <a href="#">Privacy Policy</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
