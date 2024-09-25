22779e3f4c878194f2dc98a5e9da9e25cc82bb90
asmina18 <asmina18@mail.ru>
2024-09-25 22:53:42 +0200

    opdate

--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -1,9 +1,14 @@
 import { request } from "graphql-request";
 import { useQuery } from "@tanstack/react-query";
 import { allFilms } from "../queries/getAllFilms";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Modal } from "../components/Modal/Modal";
+import { useState } from "react";
 
 export const Films = () => {
+
+  const [selectedFilm, setSelectedFilm] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
   const { data, isLoading, error } = useQuery({
     queryKey: ["allFilms"],
     queryFn: async () =>
@@ -12,6 +17,15 @@ export const Films = () => {
         allFilms
       ),
   });
+
+  const showModal = (film) => {
+    setSelectedFilm(film);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   console.log(data);
 
@@ -25,15 +39,28 @@ export const Films = () => {
 
   return (
     <div>
+      <h1>Star Wars Films</h1>
       <ul>
-        {data.allFilms.films.map((item) => {
-          return (
-            <li key={item.id}>
-              <Link to={`home/${item.id}`}>{item.title}</Link>
-            </li>
-          );
-        })}
+        {data.allFilms.films.map((film) => (
+          <li key={film.id}>
+            {/* Ved klik på film, vis modal med film-detaljer */}
+            <button onClick={() => showModal(film)}>{film.title}</button>
+          </li>
+        ))}
       </ul>
+
+      {/* Modal der viser film-detaljer */}
+      {modalOpen && (
+        <Modal>
+          <div className="modal-content">
+            <h2>{selectedFilm.title}</h2>
+            <p><strong>Episode:</strong> {selectedFilm.episodeID}</p>
+            <p><strong>Director:</strong> {selectedFilm.director}</p>
+            <p><strong>Opening Crawl:</strong> {selectedFilm.openingCrawl}</p>
+            <button onClick={closeModal}>Close Modal</button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
